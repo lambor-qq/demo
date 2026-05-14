e5bd9479f68f5f5cbcff16f1f99bf2f206775b05
root <root@10.0.3.15>
2026-05-14 20:31:32 +0800

    测试

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	r.GET("/hello", func(c *gin.Context) {
+		name := c.Query("name")
+		if name == "" {
+			name = "World"
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Hello " + name})
+	})
+
+	r.POST("/data", func(c *gin.Context) {
+		var data map[string]interface{}
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"received": data})
+	})
+
+	r.Run(":8081")
+}
